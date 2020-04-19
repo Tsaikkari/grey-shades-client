118074bba288d65b1f5980dea5e7de92edf3deb2
Tsaikkari <trospe@gmx.de>
2020-04-19 17:34:29 +0200

    Moving components into Header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import Header from "./Header";
-import Boxes from "./Boxes";
-import Greysgrid from "./Greysgrid";
-import greys from "../greys";
 
 var buttonIsClicked = "true";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Boxes 
-        isClicked={buttonIsClicked}
+      <Header 
+          isClicked={buttonIsClicked}
       />
-      {greys.map(greyLink => (
-        <Greysgrid 
-          href={greyLink.href}
-          name={greyLink.name}
-          bgColor={greyLink.bgColor}
-        />
-      ))}
     </div>
   );
 } 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import Boxes from "./Boxes";
+import Greysgrid from "./Greysgrid";
+import greys from "../greys";
 
 // TODO: create onclick function
 
 function Header(props) {
     return (
-      <header className="container-fluid" id="black-n-white-screen">
-        <div className="main-header">
-          {props.isClicked === true && <button onClick=""className="btn btn-secondary btn-lg" id="move-div">They say</button>}
-        </div>
-      </header>
+      <div>
+        <header className="container-fluid" id="black-n-white-screen">
+          <div className="main-header">
+            <button onClick=""className="btn btn-secondary btn-lg" id="move-div">They say</button>
+            <Boxes />
+            {props.isClicked === true && greys.map(greyLink => (
+              <Greysgrid 
+              href={greyLink.href}
+              name={greyLink.name}
+              bgColor={greyLink.bgColor}
+            />
+            ))}
+          </div>
+        </header>
+      </div>
     );
 }
 
